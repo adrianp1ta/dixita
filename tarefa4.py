cf68844ffb6909ea3ae36f14c6f0c46fbdbbdc5e
adrianp1ta <adrianpitadixitalizacion@gmail.com>
2025-03-10 16:23:53 +0100

    Add files via upload

---
+++ tarefa4.py
@@ -0,0 +1,6 @@
+resp = input("como te chamas? ");
+print("Guau... que nome mais interesante"  , resp);
+print("Ti es unha grande persoa"  , resp);
+print("vales moito" , resp);
+print("non hai ninguen mais elegante ca" , resp);
+print("e..." , resp , ", o mundo adorate!");
